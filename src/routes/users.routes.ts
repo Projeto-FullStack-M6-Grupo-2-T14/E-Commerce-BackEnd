d1dd4c547041cf398b5517b3771b63fef2b11043
pethe <pethersonreis@gmail.com>
2023-06-25 20:51:51 -0300

    update

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,48 @@
-import { Router } from 'express';
-import validateData from '../middlewares/validateData.middleware';
-import { entryUserSchema } from '../schemas/users.schema';
-import { createUserController, deleteUserController, listUserController, updateUserController } from '../controllers/users.controller';
-import { ensureAuthIsValidMiddleware } from '../middlewares/ensureAuthIsValid.middleware';
+import { Request, Response, Router } from "express";
+import validateData from "../middlewares/validateData.middleware";
+import { entryUserSchema } from "../schemas/users.schema";
+import {
+	createUserController,
+	deleteUserController,
+	listUserController,
+	updateUserController,
+} from "../controllers/users.controller";
+import { ensureAuthIsValidMiddleware } from "../middlewares/ensureAuthIsValid.middleware";
+import {
+	resetPassword,
+	sendEmailResetPassword,
+} from "../services/users/resetUserPassword.service";
+import { Repository } from "typeorm";
+import { User } from "../entities";
+import { AppDataSource } from "../data-source";
 
-const usersRoutes: Router = Router()
+const usersRoutes: Router = Router();
 
-usersRoutes.post('', validateData(entryUserSchema), createUserController)
-usersRoutes.get('', listUserController)
-usersRoutes.delete('/:id', ensureAuthIsValidMiddleware, deleteUserController)
-usersRoutes.patch('/:id', ensureAuthIsValidMiddleware, updateUserController)
+const sendEmailUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const email = req.body.email;
+	sendEmailResetPassword(email);
 
-export default usersRoutes
+	return res.status(200).json("Email sent!");
+};
+const resetUserPassController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const password = req.body.password;
+
+	resetPassword(password, "tokenReset");
+
+	return res.status(200).json("Password changed");
+};
+
+usersRoutes.post("", validateData(entryUserSchema), createUserController);
+usersRoutes.post("/sendemail", sendEmailUserController);
+usersRoutes.post("/resetpass/:resettoken", resetUserPassController);
+usersRoutes.get("", listUserController);
+usersRoutes.delete("/:id", ensureAuthIsValidMiddleware, deleteUserController);
+usersRoutes.patch("/:id", ensureAuthIsValidMiddleware, updateUserController);
+
+export default usersRoutes;
