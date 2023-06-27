149fc52aac1c22c245a174d19bbfe2f3e30d842d
pethe <pethersonreis@gmail.com>
2023-06-27 14:38:10 -0300

    fix: send and resetpass controlers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,52 +5,91 @@ import { deleteUserServices } from "../services/users/deleteUser.service";
 import { updateUserServices } from "../services/users/updateUser.service";
 import { TUserPartial, TUserRequest } from "../interfaces/users.interface";
 import { retrieveUserServices } from "../services/users/retrieveUser.service";
+import {
+	resetPassword,
+	sendEmailResetPassword,
+} from "../services/users/resetUserPassword.service";
 
-const createUserController = async (req: Request, res: Response): Promise<Response> => {
+const createUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
 	const userData: TUserRequest = req.body;
 	const newUser = await createUserServices(userData);
 
 	return res.status(201).json(newUser);
 };
 
-const listUserController = async (req: Request, res: Response): Promise<Response> => {
-
+const listUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
 	const users = await listUserServices();
-  
+
 	return res.json(users);
-  
 };
-  
-const updateUserController = async (req: Request, res: Response): Promise<Response> => {
-  
+
+const updateUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
 	const userData: TUserPartial = req.body;
 	const userId: number = parseInt(req.params.id);
 
-	const updateUser = await updateUserServices(userData, userId)
-  
+	const updateUser = await updateUserServices(userData, userId);
+
 	return res.status(200).json(updateUser);
-  
 };
-  
-const deleteUserController = async (req: Request, res: Response): Promise<Response> => {
-  
+
+const deleteUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
 	const userId: number = parseInt(req.params.id);
 
-	await deleteUserServices(userId)
-  
+	await deleteUserServices(userId);
+
 	return res.status(204).send();
-	
 };
 
-const retrieveUserController = async (req: Request, res: Response): Promise<Response> => {
+const retrieveUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const userId: number = parseInt(req.params.id);
 
-	const userId: number = parseInt(req.params.id)
-	
 	const users = await retrieveUserServices(userId);
-  
+
 	return res.json(users);
-  
 };
 
-export { createUserController, listUserController, deleteUserController, updateUserController, retrieveUserController }
+const sendEmailUserController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const email = req.body.email;
+	sendEmailResetPassword(email);
 
+	return res.status(200).json("Email sent!");
+};
+const resetUserPassController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const password = req.body.newPass,
+		reset_token = req.query.reset_token as string;
+
+	resetPassword(password, reset_token);
+
+	return res.status(200).json("Password changed");
+};
+
+export {
+	createUserController,
+	listUserController,
+	deleteUserController,
+	updateUserController,
+	retrieveUserController,
+	sendEmailUserController,
+	resetUserPassController,
+};
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,45 +1,23 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import validateData from "../middlewares/validateData.middleware";
 import { userSchemaRequest } from "../schemas/users.schema";
 import {
 	createUserController,
 	deleteUserController,
 	listUserController,
+	resetUserPassController,
+	sendEmailUserController,
 	updateUserController,
 } from "../controllers/users.controller";
 import ensureAuthIsValidMiddleware from "../middlewares/ensureAuthIsValid.middleware";
-import {
-	resetPassword,
-	sendEmailResetPassword,
-} from "../services/users/resetUserPassword.service";
 
 const usersRoutes: Router = Router();
 
-const sendEmailUserController = async (
-	req: Request,
-	res: Response
-): Promise<Response> => {
-	const email = req.body.email;
-	sendEmailResetPassword(email);
-
-	return res.status(200).json("Email sent!");
-};
-const resetUserPassController = async (
-	req: Request,
-	res: Response
-): Promise<Response> => {
-	const password = req.body.newPass,
-		reset_token = req.query.reset_token as string;
-
-	resetPassword(password, reset_token);
-
-	return res.status(200).json("Password changed");
-};
-
+usersRoutes.get("", listUserController);
+// usersRoutes.get("")
 usersRoutes.post("", validateData(userSchemaRequest), createUserController);
 usersRoutes.post("/sendemail", sendEmailUserController);
 usersRoutes.post("/resetpassword", resetUserPassController);
-usersRoutes.get("", listUserController);
 usersRoutes.delete("/:id", ensureAuthIsValidMiddleware, deleteUserController);
 usersRoutes.patch("/:id", ensureAuthIsValidMiddleware, updateUserController);
 
