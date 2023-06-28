ebf6ff57d9372823a9be4a0a030c2e2d0566ab96
Junior Santos <nuno.ribeiro05@hotmail.com>
2023-06-28 18:34:21 -0300

    feat: add userPosterRoutes

--- src/routes/userPosters.routes.ts
+++ src/routes/userPosters.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import listUserPostersController from "../controllers/userPosters.controllers";
+import ensureAuthIsValidMiddleware from "../middlewares/ensureAuthIsValid.middleware";
+import isUserOrOwner from "../middlewares/ensureIsOwner.middleware";
 
 
 const userPosterRoutes: Router = Router();
 
-userPosterRoutes.get("/:id", listUserPostersController);
+userPosterRoutes.get("/:id", ensureAuthIsValidMiddleware, isUserOrOwner, listUserPostersController);
 
 export default userPosterRoutes
