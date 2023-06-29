b04018fc5505365b379f6555e8f4c07ecb4caaf1
pethe <pethersonreis@gmail.com>
2023-06-29 13:45:10 -0300

    fix: expires in token

--- src/controllers/posters.controllers.ts
+++ src/controllers/posters.controllers.ts
@@ -4,49 +4,71 @@ import updatePosterService from "../services/posters/updatePoster.service";
 import retrievePosterServices from "../services/posters/retrievePoster.service";
 import deletePostersService from "../services/posters/deletePoster.service";
 import createPosterServices from "../services/posters/createPoster.service";
-import { TEntryPoster, TExitPoster, TListPosters } from "../interfaces/posters.interface";
+import {
+	TEntryPoster,
+	TExitPoster,
+	TListPosters,
+} from "../interfaces/posters.interface";
 
+const listAllPostersController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const getPosters: TListPosters = await listAllPostersService();
 
-const listAllPostersController = async (req: Request, res: Response): Promise<Response> => {
-  const getPosters: TListPosters = await listAllPostersService();
-
-  return res.status(200).json(getPosters);
+	return res.status(200).json(getPosters);
 };
 
-const retrievePosterController = async (req: Request, res: Response): Promise<Response> => {
-  const posterId = parseInt(req.params.id);
-  const poster: TExitPoster = await retrievePosterServices(posterId);
+const retrievePosterController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const posterId = parseInt(req.params.id);
+	const poster: TExitPoster = await retrievePosterServices(posterId);
 
-  return res.status(200).json(poster);
+	return res.status(200).json(poster);
 };
 
-const deletePostersController = async (req: Request, res: Response): Promise<Response> => {
-  const posterId = req.params.id;
-  await deletePostersService(Number(posterId));
+const deletePostersController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const posterId = req.params.id;
+	await deletePostersService(Number(posterId));
 
-  return res.status(204).send();
+	return res.status(204).send();
 };
 
-const createPosterController = async (req: Request, res: Response): Promise<Response> => {
+const createPosterController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
 	const posterData = req.body;
-  const userId: number = res.locals.userId
+	const userId: number = res.locals.userId;
+
 	const newPoster = await createPosterServices(posterData, userId);
 
 	return res.status(201).json(newPoster);
 };
 
-const updatePosterController = async (req: Request, res: Response): Promise<Response> => {
-  const newPosterData: TEntryPoster = req.body
-  const posterId: number = res.locals.id
-  const updatedPoster: TExitPoster = await updatePosterService(newPosterData, posterId)
+const updatePosterController = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	const newPosterData: TEntryPoster = req.body;
+	const posterId: number = res.locals.id;
+	const updatedPoster: TExitPoster = await updatePosterService(
+		newPosterData,
+		posterId
+	);
 
-  return res.status(200).json(updatedPoster)
-}
+	return res.status(200).json(updatedPoster);
+};
 
 export {
-  listAllPostersController,
-  deletePostersController,
-  retrievePosterController,
-  createPosterController,
-  updatePosterController
+	listAllPostersController,
+	deletePostersController,
+	retrievePosterController,
+	createPosterController,
+	updatePosterController,
 };
