8ca438baf215df3855ad53f460407e6612182bfa
Junior Santos <nuno.ribeiro05@hotmail.com>
2023-06-29 16:41:56 -0300

    feat: adicionando algumas tipagens e verificações

--- src/routes/userPosters.routes.ts
+++ src/routes/userPosters.routes.ts
@@ -3,7 +3,6 @@ import listUserPostersController from "../controllers/userPosters.controllers";
 import ensureAuthIsValidMiddleware from "../middlewares/ensureAuthIsValid.middleware";
 import isUserOrOwner from "../middlewares/ensureIsOwner.middleware";
 
-
 const userPosterRoutes: Router = Router();
 
 userPosterRoutes.get("/:id", ensureAuthIsValidMiddleware, isUserOrOwner, listUserPostersController);
