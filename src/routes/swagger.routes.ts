feab10557c8c4e02297dc097c2c0ed6103e2367b
mayson <pethersonreis@gmail.com>
2025-07-18 00:35:23 -0300

    fix: swaggerRoutes

--- src/routes/swagger.routes.ts
+++ src/routes/swagger.routes.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDoc from "../swagger.json" with { type: "json" };
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { resolve, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const swaggerPath = resolve(__dirname, "../swagger.json");
+
+const swaggerDoc = JSON.parse(readFileSync(swaggerPath, "utf-8"));
 
 const swaggerRoutes: Router = Router();
 
