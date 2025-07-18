a1afde0abbb200ef5b10ee1761bb3044bb696fda
mayson <pethersonreis@gmail.com>
2025-07-17 22:48:35 -0300

    fix: import type in package json

--- src/schemas/address.schema.ts
+++ src/schemas/address.schema.ts
@@ -1,23 +1,17 @@
 import { z } from "zod";
 
 export const addressSchema = z.object({
-    id: z.number(),
-    zipcode: z.string().max(8),
-    state: z.string().max(80),
-    city: z.string().max(120),
-    street: z.string().max(150),
-    number: z.string().max(8),
-    complement: z.string().max(200)
-})
+	id: z.number(),
+	zipcode: z.string().max(8),
+	state: z.string().max(80),
+	city: z.string().max(120),
+	street: z.string().max(150),
+	number: z.string().max(8),
+	complement: z.string().max(200),
+});
 
 export const addressSchemaRequest = addressSchema.omit({
-    id: true,
-    user: true,
-  });
+	id: true,
+});
 
 export const partialAddressSchema = addressSchemaRequest.partial();
-
-
-
-
-
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import "dotenv/config";
 import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize()
-  .then(() => {
-    console.log("database is connected");
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on ${PORT}`);
-    });
-  })
-  .catch((error) => console.log(error));
+	.then(() => {
+		console.log("database is connected");
+		const PORT = process.env.PORT || 3008;
+		app.listen(PORT, () => {
+			console.log(`Server is running on ${PORT}`);
+		});
+	})
+	.catch((error) => console.log(error));
